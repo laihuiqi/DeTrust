4cadc2200746bb5029b56de77e673c40c7dc8115
Jing Hao <jinghaoong@gmail.com>
2023-10-17 16:24:59 +0800

    Move TrustScore.sol back to contracts folder

--- contracts/dispute-resolution-v1/Dispute.sol
+++ contracts/dispute-resolution-v1/Dispute.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-import "../trust-score/TrustScore.sol";
+import "../TrustScore.sol";
 
 contract Dispute {
     // Basic variables
